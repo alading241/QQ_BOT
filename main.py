e85c550fc826cf030d2468dd0fc3096aca5300af
rentianyu <1782898491@qq.com>
2021-06-25 23:25:58 +0800

    003.增加main.py内容

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ text, image, record, video, location = 'text', 'image', 'record', 'video', 'loca
 
 
 @bot_server.route('/api/message', methods=['POST'])
-# 路径是你在酷Q配置文件里自定义的
+# 路径是你在配置文件里自定义的
 def server():
     '''主要函数'''
     data = request.get_data().decode('utf-8')
@@ -43,9 +43,47 @@ def server():
                     send('你好')
 
         # 如果消息是群消息
-        elif message_type == 'private':
+        elif message_type == 'group':
             # 定义发送群
             group_id = str(data['group_id'])
+            if msg == '一言':
+                sendg(alapi('hitokoto'), group_id)
+            elif msg == '毒鸡汤':
+                sendg(alapi('soul'), group_id)
+            elif msg == '舔狗日记':
+                sendg(alapi('dog'), group_id)
+            elif msg == '诗词':
+                sendg(alapi('shici'), group_id)
+            elif msg == '舔狗日记':
+                sendg(alapi('dog'), group_id)
+            elif msg == '笑话':
+                sendg(alapi('joke/random'), group_id)
+            elif msg == '美文':
+                sendg(alapi('mryw/random'), group_id)
+            elif msg == '歇后语':
+                sendg(alapi('xhy/random'), group_id)
+            elif msg == '谜语':
+                sendg(alapi('riddle/random'), group_id)
+            elif msg == '早报':
+                sendg(alapi('zaobao'), group_id, image)
+            elif msg == '历史今天':
+                sendg(alapi('eventHistory'), group_id)
+            elif msg in ['ACG', 'acg', 'ACG图片']:
+                sendg(alapi('acg'), group_id, image)
+            elif msg in ['bing', 'BING', '必应', '必应日图']:
+                sendg(alapi('bing'), group_id, image)
+            elif msg[:2] == '字典':
+                sendg(alapi('word', msg[3]), group_id)
+            elif msg[:2] == '词典':
+                sendg(alapi('ciword', msg[3:]), group_id)
+            elif msg[:2] == '成语':
+                sendg(alapi('idiom', msg[3:]), group_id)
+            # 基金估值查询
+            elif msg.isdigit and len(msg) == 6:
+                sendg(fund_img(msg), user_id, image)
+
+            else:
+                sendg(alapi('help'), group_id)
 
 
     return ''
